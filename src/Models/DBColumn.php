6b08a4b1c0f8f2249e512e9e7b3ba33681648cee
angujo <barrack.omondi@wwarn.org>
2022-12-27 11:34:26 +0000

    consider date as datetime

--- src/Models/DBColumn.php
+++ src/Models/DBColumn.php
@@ -20,40 +20,43 @@ use function Angujo\Lareloquent\str_equal;
 
 class DBColumn extends DBInterface
 {
-    public string      $table_name;
-    public string      $column_name;
+    public string $table_name;
+    public string $column_name;
     public string|null $referenced_table_name;
     public string|null $column_type;
     public string|null $referenced_column_name;
-    public string      $column_comment;
-    public int|null    $ordinal_position;
-    public int|null    $character_maximum_length = null;
-    public int|null    $numeric_scale            = null;
+    public string $column_comment;
+    public int|null $ordinal_position;
+    public int|null $character_maximum_length = null;
+    public int|null $numeric_scale = null;
     public string|null $column_default;
-    public string      $data_type;
-    public bool        $is_nullable;
-    public bool        $is_primary;
-    public bool        $is_unique;
-    public bool        $increments;
-    public bool        $is_updating;
+    public string $data_type;
+    public bool $is_nullable;
+    public bool $is_primary;
+    public bool $is_unique;
+    public bool $increments;
+    public bool $is_updating;
 
-    private ValueCast|null $valueCast   = null;
-    private ?DataType      $id_type;
-    private array          $_validation = [];
+    private ValueCast|null $valueCast = null;
+    private ?DataType $id_type;
+    private array $_validation = [];
 
-    public function getValidation()
-    : array
+    public function getValidation(): array
     {
         if (empty($this->column_comment)) return [];
         if (!empty($this->_validation)) return $this->_validation;
         $raw_valids = explode(';', preg_replace('/^(.*?)validation(\s+)?:(\s+)?\{(.*?)\}(.*?)$/', '$4', $this->column_comment));
 
-        $raw_valids = array_values(array_map(function($v){ return array_map('trim', explode(':', $v)); }, $raw_valids));
-        $raw_valids = array_combine(array_column($raw_valids, 0), array_map(function($v){
+        $raw_valids = array_values(array_map(function ($v) {
+            return array_map('trim', explode(':', $v));
+        }, $raw_valids));
+        $raw_valids = array_combine(array_column($raw_valids, 0), array_map(function ($v) {
             array_shift($v);
             return implode('', $v);
         }, $raw_valids));
-        return $this->_validation = array_filter($raw_valids, function($k){ return array_key_exists(strtolower($k), BaseRequest::$default_messages); }, ARRAY_FILTER_USE_KEY);
+        return $this->_validation = array_filter($raw_valids, function ($k) {
+            return array_key_exists(strtolower($k), BaseRequest::$default_messages);
+        }, ARRAY_FILTER_USE_KEY);
     }
 
     public function cast()
@@ -69,15 +72,14 @@ class DBColumn extends DBInterface
     /**
      * @throws \Exception
      */
-    public function getEnum()
-    : ?DBEnum
+    public function getEnum(): ?DBEnum
     {
         return Enumer::getEnum($this->connection_name, $this->column_name);
     }
 
     public function constantName()
     {
-        return strtoupper(LarEloquent::config()->constant_column_prefix.preg_replace(['/(^[^a-zA-Z]+)|([^a-zA-Z\d]+$)/', '/[^a-zA-Z0-9]+/'], ['', '_'], $this->column_name));
+        return strtoupper(LarEloquent::config()->constant_column_prefix . preg_replace(['/(^[^a-zA-Z]+)|([^a-zA-Z\d]+$)/', '/[^a-zA-Z0-9]+/'], ['', '_'], $this->column_name));
     }
 
     public function constantProperty()
@@ -123,14 +125,14 @@ class DBColumn extends DBInterface
 
     public function docType()
     {
-        return $this->dataType().($this->is_nullable ? '|null' : '');
+        return $this->dataType() . ($this->is_nullable ? '|null' : '');
     }
 
     public function defaultValue()
     {
         if (is_null($this->column_default)) return null;
         return match ($this->PhpDataType()) {
-            DataType::DATETIME => str_contains(strtoupper($this->column_default), 'CURRENT_TIMESTAMP') ? null : $this->column_default,
+            DataType::DATETIME=> str_contains(strtoupper($this->column_default), 'CURRENT_TIMESTAMP') ? null : $this->column_default,
             DataType::BOOL => (bool)$this->column_default,
             DataType::INT => intval($this->column_default),
             DataType::FLOAT => floatval($this->column_default),
@@ -143,8 +145,7 @@ class DBColumn extends DBInterface
         return (str_equal(DataType::DATETIME->value, $this->PhpDataType()->value) ? basename(Carbon::class) : $this->PhpDataType()->value);
     }
 
-    public function PhpDataType()
-    : DataType
+    public function PhpDataType(): DataType
     {
         switch ($this->data_type) {
             case SQLType::TINYINT->value:
@@ -161,6 +162,7 @@ class DBColumn extends DBInterface
                 return DataType::FLOAT;
             case SQLType::DATETIME->value:
             case SQLType::TIMESTAMP->value:
+            case SQLType::DATE->value:
                 return DataType::DATETIME;
             case SQLType::JSON->value:
                 return DataType::JSON;
@@ -176,7 +178,6 @@ class DBColumn extends DBInterface
             case SQLType::MEDIUMBLOB->value:
             case SQLType::TIME->value:
             case SQLType::LONGBLOB->value:
-            case SQLType::DATE->value:
             case SQLType::GEOMETRY->value:
             case SQLType::VARCHAR->value:
             case SQLType::LONGTEXT->value:
@@ -185,8 +186,7 @@ class DBColumn extends DBInterface
         }
     }
 
-    public function tsDataType()
-    : TSType
+    public function tsDataType(): TSType
     {
         return match ($this->data_type) {
             SQLType::TINYINT->value, SQLType::BOOL->value, SQLType::BOOLEAN->value => TSType::BOOLEAN,
@@ -199,8 +199,7 @@ class DBColumn extends DBInterface
         };
     }
 
-    public function tsTypeValue()
-    : string
+    public function tsTypeValue(): string
     {
         return match ($this->tsDataType()) {
             TSType::TUPLE, TSType::ARRAY => 'Array<any>',
@@ -211,7 +210,7 @@ class DBColumn extends DBInterface
 
     public function tsPropertyName()
     {
-        return $this->column_name.($this->is_nullable || $this->increments ? '?' : '');
+        return $this->column_name . ($this->is_nullable || $this->increments ? '?' : '');
     }
 
     public function tsValue()
@@ -230,67 +229,57 @@ class DBColumn extends DBInterface
         };
     }
 
-    public function isMacAddress()
-    : bool
+    public function isMacAddress(): bool
     {
         return DataType::MAC_ADDRESS === $this->getTypeId();
     }
 
-    public function isUUID()
-    : bool
+    public function isUUID(): bool
     {
         return DataType::UUID === $this->getTypeId();
     }
 
-    public function isURL()
-    : bool
+    public function isURL(): bool
     {
         return DataType::URL === $this->getTypeId();
     }
 
-    public function isEmail()
-    : bool
+    public function isEmail(): bool
     {
         return DataType::EMAIL === $this->getTypeId();
     }
 
-    public function isIpAddress()
-    : bool
+    public function isIpAddress(): bool
     {
         return DataType::IP === $this->getTypeId();
     }
 
-    public function isArray()
-    : bool
+    public function isArray(): bool
     {
         return in_array(DataType::ARRAY, [$this->getTypeId(), $this->PhpDataType()]);
     }
 
-    public function isImage()
-    : bool
+    public function isImage(): bool
     {
         return DataType::IMAGE === $this->getTypeId();
     }
 
-    public function isFile()
-    : bool
+    public function isFile(): bool
     {
         return DataType::FILE === $this->getTypeId();
     }
 
-    public function isJson()
-    : bool
+    public function isJson(): bool
     {
         return in_array(DataType::JSON, [$this->getTypeId(), $this->PhpDataType()]);
     }
 
-    protected function getTypeId()
-    : DataType
+    protected function getTypeId(): DataType
     {
         if (isset($this->id_type)) return $this->id_type;
         $identifiers = is_array(LarEloquent::config()->identified_columns) ? LarEloquent::config()->identified_columns : [];
-        $permitted   = [DataType::ARRAY->value, DataType::IMAGE->value, DataType::FILE->value, DataType::JSON->value, DataType::IP->value,
-                        DataType::UUID->value, DataType::URL->value, DataType::EMAIL->value, DataType::MAC_ADDRESS->value,];
+        $permitted = [DataType::ARRAY->value, DataType::IMAGE->value, DataType::FILE->value, DataType::JSON->value, DataType::IP->value,
+            DataType::UUID->value, DataType::URL->value, DataType::EMAIL->value, DataType::MAC_ADDRESS->value,];
         foreach ($identifiers as $key => $identities) {
             if (!(is_array($identities) && in_array($key, $permitted))) continue;
             foreach ($identities as $identity) {
@@ -326,6 +315,8 @@ class DBColumn extends DBInterface
         if (!in_array($this->PhpDataType(), [DataType::INT, DataType::STRING, DataType::FLOAT])) return null;
         return DataType::STRING == $this->PhpDataType() ?
             $this->character_maximum_length :
-            floatval("1".implode('', array_map(function($v){ return "0"; }, range(1, $this->character_maximum_length - $this->numeric_scale)))) - 1;
+            floatval("1" . implode('', array_map(function ($v) {
+                    return "0";
+                }, range(1, $this->character_maximum_length - $this->numeric_scale)))) - 1;
     }
 }
