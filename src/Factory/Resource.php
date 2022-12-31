3d99ac90e1fc399b342bed8e476fa14bee522e7e
angujo <barrack.omondi@wwarn.org>
2022-12-31 12:46:26 +0000

    remove deleted at from resources by default

--- src/Factory/Resource.php
+++ src/Factory/Resource.php
@@ -24,16 +24,16 @@ class Resource extends FileCreator
 
     public function __construct(DBTable $table, array $columns)
     {
-        $this->columns      = $columns;
-        $this->table        = $table;
-        $this->name         = model_name(LarEloquent::config()->base_resource_prefix.'_'.model_name($this->table_name=$this->table->name).'_'.LarEloquent::config()->resource_suffix);
+        $this->columns = $columns;
+        $this->table = $table;
+        $this->name = model_name(LarEloquent::config()->base_resource_prefix . '_' . model_name($this->table_name = $this->table->name) . '_' . LarEloquent::config()->resource_suffix);
         $this->parent_class = JsonResource::class;
-        $this->dir          = Path::Combine(LarEloquent::config()->resources_dir, model_name(LarEloquent::config()->base_resource_prefix));
-        $this->namespace    = implode('\\', [LarEloquent::config()->resource_namespace, model_name(LarEloquent::config()->base_resource_prefix)]);
+        $this->dir = Path::Combine(LarEloquent::config()->resources_dir, model_name(LarEloquent::config()->base_resource_prefix));
+        $this->namespace = implode('\\', [LarEloquent::config()->resource_namespace, model_name(LarEloquent::config()->base_resource_prefix)]);
         parent::__construct();
         $this->class->setAbstract(true)
-                    ->setDocBlock((new DocBlockGenerator('Base Resource class for the '.model_name($table->name).' model.'))
-                                      ->setLongDescription('This file should not be modified as it is to be overwritten when regenerated!'));
+            ->setDocBlock((new DocBlockGenerator('Base Resource class for the ' . model_name($table->name) . ' model.'))
+                ->setLongDescription('This file should not be modified as it is to be overwritten when regenerated!'));
     }
 
     private function asArrayMethod()
@@ -42,25 +42,27 @@ class Resource extends FileCreator
             ->setAbstract(true)
             ->setParameter(new ParameterGenerator('request'))
             ->setDocBlock((new DocBlockGenerator('Method to extend default resource variables.', 'Return array with extra or overwriting keys to be merged with default model resource variables.'))
-                              ->setTag(new ParamTag('request', ['Request']))
-                              ->setTag(new ReturnTag('array')))
+                ->setTag(new ParamTag('request', ['Request']))
+                ->setTag(new ReturnTag('array')))
             ->setReturnType('array');
     }
 
     private function toArrayMethod()
     {
-        $columns=array_filter($this->columns,fn(DBColumn $column)=>!(array_key_exists($this->table->name,LarEloquent::config()->hidden_columns) && in_array($column->column_name,LarEloquent::config()->hidden_columns[$this->table->name])));
-        $body   = ['$defaults = '.
-                   (new ValueGenerator(array_combine(
-                                           array_map(fn(DBColumn $column) => $column->column_name, $columns),
-                                           array_map(fn(DBColumn $column) => (new ValueGenerator("\$this->{$column->column_name}"))
-                                               ->setType(ValueGenerator::TYPE_CONSTANT), $columns)))).';'];
+        $columns = array_filter($this->columns,
+            fn(DBColumn $column) => !in_array($column->column_name, LarEloquent::config()->soft_delete_columns)
+                && !(array_key_exists($this->table->name, LarEloquent::config()->hidden_columns) && in_array($column->column_name, LarEloquent::config()->hidden_columns[$this->table->name])));
+        $body = ['$defaults = ' .
+            (new ValueGenerator(array_combine(
+                array_map(fn(DBColumn $column) => $column->column_name, $columns),
+                array_map(fn(DBColumn $column) => (new ValueGenerator("\$this->{$column->column_name}"))
+                    ->setType(ValueGenerator::TYPE_CONSTANT), $columns)))) . ';'];
         $body[] = 'return array_merge($defaults, $this->asArray($request));';
         return (new MethodGenerator('toArray'))
             ->setDocBlock((new DocBlockGenerator())
-                              ->setShortDescription('Transform the resource into an array.')
-                              ->setTag(new ParamTag('request', ['Request']))
-                              ->setTag(new ReturnTag('array')))
+                ->setShortDescription('Transform the resource into an array.')
+                ->setTag(new ParamTag('request', ['Request']))
+                ->setTag(new ReturnTag('array')))
             ->setParameter('request')
             ->setBody(implode("\n", $body))
             ->setReturnType('array');
@@ -70,8 +72,8 @@ class Resource extends FileCreator
     {
         $m = new self($table, $columns);
         $m->class->addUse(\Illuminate\Http\Request::class)
-                 ->addMethodFromGenerator($m->asArrayMethod())
-                 ->addMethodFromGenerator($m->toArrayMethod());
+            ->addMethodFromGenerator($m->asArrayMethod())
+            ->addMethodFromGenerator($m->toArrayMethod());
         $m->_write();
     }
 }
